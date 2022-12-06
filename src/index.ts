695a149e03095028c2366e31ef63cc1d7616880f
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-06 11:58:21 -0300

    feat: change to cors library

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@ import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 import { router } from './router';
+import cors from 'cors';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
     const PORT = 3001;
 
-    app.use((req, res, next) => {
-      res.setHeader('Access-Controll-Allow-Origin', 'http://localhost:5173/');
-      res.setHeader('Access-Controll-Allow-Methods', '*');
-      res.setHeader('Access-Controll-Allow-Headers', '*');
-      next();
-    });
+    app.use(cors({
+      allowedHeaders: '*',
+      origin: 'http://localhost:5173',
+      methods: '*'
+    }));
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
     app.use(express.json());
     app.use(router);
